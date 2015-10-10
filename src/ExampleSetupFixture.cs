b5a81c0bda78165b0e45313830e15b5d4fcc5921
prwh <paulhorridge@gmail.com>
2015-10-10 11:43:34 +0100

    Example use added.

---
+++ src/ExampleSetupFixture.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Testing.RabbitMq
+{
+    [SetUpFixture]
+    public class ExampleSetupFixture
+    {
+        private RabbitMqInstance _rabbitMqInstance;
+
+        [SetUp]
+        public void Setup()
+        {
+            _rabbitMqInstance = new RabbitMqInstance();
+            _rabbitMqInstance.Start();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _rabbitMqInstance.Stop();
+        }
+    }
+}
---
+++ src/Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using RabbitMQ.Client;
+
+namespace Testing.RabbitMq
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void RabbitMqInstanceIsRunning()
+        {
+            var connectionFactory = new ConnectionFactory { Port = 5675 };
+
+            var instance = connectionFactory.CreateConnection();
+
+            Assert.That(instance.IsOpen, Is.True.After(5000, 100));
+        }
+    }
+}
